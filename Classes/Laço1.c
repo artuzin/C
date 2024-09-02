19a0ab9356772d7b3bfece43d55efda759f201e3
artuzin <arturboudens@gmail.com>
2024-09-02 11:00:30 -0300

    Aula 8

---
+++ Classes/Laço1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int i, n, r;
+int main(){
+    i=1;
+    while(i <= 5){
+        printf("Digite um numero: ");
+        scanf("%d", &n);
+        r = n * 3;
+        printf("%d \n",r );
+        i = i + 1;
+    }
+}
