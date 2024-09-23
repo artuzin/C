fbd81f041d8ad2a6e2382e8e3185d86647c7c070
artuzin <arturboudens@gmail.com>
2024-09-23 11:35:38 -0300

    Exercicio 2

--- Classes/Lista/2.c
+++ Classes/Lista/2.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
+
 const int max = 10;
+int i, z;
+
 int main(){
-    int v[max];
-    int ref;
+    int v[max], ref, f[max]; 
+    
     printf("Digite um valor de referência inteiro: ");
-    scanf(%d, &ref); 
-     for(int i = 0 ; i < max; i++){
+    scanf("%d", &ref); 
+    
+    for(i = 0 ; i < max; i++){
         printf("Digite um valor inteiro: ");
-        scanf(%d, &v[i]);
-     }
+        scanf("%d", &v[i]);
+        }
+    for(i = 0; i < max; i++){
+            if(v[i] > ref){
+                f[i] = v[i];
+            }
+        }
+    for(i = 0; i < max; i++){ 
+     printf("%d", f[i]);}
+     return 0;
 }
