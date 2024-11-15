84713eb8c63509db6059c49e51e43d863902701f
artuzin <85762182+artuzin@users.noreply.github.com>
2024-11-15 20:20:02 -0300

    a

--- Lista3C/4.c
+++ Lista3C/4.c
@@ -4,7 +4,7 @@
 
 int imprimir(int *v){
     printf("Valores do vetor: %d\n", *v);
-    v = v + 1;
+    v = v + 1; //avaça uma posição dentro do ponteiro, indo para o proximo elemento do vetor
     printf("Valores do vetor: %d\n", *v);
     v = v + 1;
     printf("Valores do vetor: %d\n", *v);
@@ -14,8 +14,8 @@ int main(){
     int *v = (int*)malloc(3 * sizeof(int));
     int vetor[] = {4, 77, 123};
 
-    memcpy(v, vetor, 3 * sizeof(int));
-
+    memcpy(v, vetor, 3 * sizeof(int)); //copia o numero de bytes declarados no terceiro parametro do segundo parametro ao primeiro (do array ate o ponteiro)
+    
     imprimir(v);
 
     return 0;
